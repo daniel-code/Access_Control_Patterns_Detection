11d17d16d16577f00fe0125f8f6aa914af822ea1
蘇彥儒 <daniel78761215@gmail.com>
2020-05-14 22:08:56 +0800

    update staytime_demo.py uncomment

--- staytime_demo.py
+++ staytime_demo.py
@@ -7,18 +7,18 @@ if __name__ == '__main__':
     ENCODE_TYPE = 2
 
     # read access control data
-    raw_data = pd.read_csv('test_data1.csv')
+    raw_data = pd.read_csv('test_data/test_data1.csv')
     raw_data['datetime'] = pd.to_datetime(raw_data['datetime'])
     # create staytime model
     staytime_model = StayTimeModel(encode_type=ENCODE_TYPE)
     # encode raw data into [previous staytime, staytime, time of day]
     encode_data, data = staytime_model.data_encoding(raw_data)
     # using modified K-mean clustering
-    # staytime_model.fit(data=encode_data)
+    staytime_model.fit(data=encode_data)
     # save each group info.
-    # staytime_model.save_model(FILE_PATH)
+    staytime_model.save_model(FILE_PATH)
     # load pre-trained group info.
-    # staytime_model.load_model(FILE_PATH)
+    staytime_model.load_model(FILE_PATH)
     # predicted access control is out of patterns
     y_pred, labels = staytime_model.predict(data=encode_data)
     # access control pattern visualisation
